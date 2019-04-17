b73eae3be57036fad22a5782df1f9eb9c7ca3364
Che Huu <chequochuu@yahoo.com.vn>
2019-04-17 10:40:55 +0700

    reduce NNN, use different buck score

--- src/algorithm.h
+++ src/algorithm.h
@@ -1,3 +1,3 @@
 void dfs_hamiltonian(int x, uint32_t depth, uint32_t *E, int *head, uint32_t *next, int *mark, uint32_t n_v, 
-		uint32_t *res, uint32_t *best_res, uint32_t *best_n_res);
+		uint32_t *res, uint32_t *best_res, uint32_t *best_n_res, uint32_t *listV);
 
