e9f2dfd4b333578c198765e547b14733e42d8e2e
Duy Tan <ptdtan@gmail.com>
2019-07-26 07:54:56 -0700

    add header for fastg

---
+++ src/fastg.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fastg.h
+ *
+ *    Description:  Prototype of loading FASTG graph function
+ *
+ *        Version:  1.0
+ *        Created:  07/26/2019 07:51:27 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Phan Thai Duy Tan (ptdtan), tan@bioturing.com
+ *   Organization:  BioTuring Dev Team
+ *
+ * =====================================================================================
+ */
+
+#include "assembly_graph.h"
+
+void load_asm_graph_fastg(struct asm_graph_t *g, const char *path, int ksize );
+
