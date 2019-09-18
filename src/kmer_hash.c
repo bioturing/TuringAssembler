dd36994784c1e7f36f2cc99af61decce44924c38
JATC1024 <trananhhao1024@gmail.com>
2019-09-17 22:25:34 -0700

    Improve perfomance of kmer_hash, do minor changes in resolve dump jungles

--- src/kmer_hash.c
+++ src/kmer_hash.c
@@ -10,19 +10,19 @@ khash_t(kmer_int) *get_kmer_hash(char *r1_path, char *r2_path, int ksize)
 	khash_t(kmer_int) *res = kh_init(kmer_int);
 
 	for (int i = 0; i < rlist1.n; ++i){
-		count_hash_from_read(rlist1.reads[i].seq, ksize, res);
+		count_hash_from_seq(rlist1.reads[i].seq, ksize, res);
 		char *rv = (char *) calloc(rlist1.reads[i].len + 1, sizeof(char));
 		strcpy(rv, rlist1.reads[i].seq);
 		flip_reverse(rv);
-		count_hash_from_read(rv, ksize, res);
+		count_hash_from_seq(rv, ksize, res);
 		free(rv);
 	}
 	for (int i = 0; i < rlist2.n; ++i){
-		count_hash_from_read(rlist2.reads[i].seq, ksize, res);
+		count_hash_from_seq(rlist2.reads[i].seq, ksize, res);
 		char *rv = (char *) calloc(rlist2.reads[i].len + 1, sizeof(char));
 		strcpy(rv, rlist2.reads[i].seq);
 		flip_reverse(rv);
-		count_hash_from_read(rv, ksize, res);
+		count_hash_from_seq(rv, ksize, res);
 		free(rv);
 	}
 	read_list_destroy(&rlist1);
@@ -30,22 +30,22 @@ khash_t(kmer_int) *get_kmer_hash(char *r1_path, char *r2_path, int ksize)
 	return res;
 }
 
-void count_hash_from_read(char *seq, int ksize, khash_t(kmer_int) *h)
+void count_hash_from_seq(char *seq, int ksize, khash_t(kmer_int) *h)
 {
 	if (strlen(seq) < ksize)
 		return;
 	uint64_t power = 1;
 	for (int i = 0; i < ksize - 1; ++i)
-		power *= 4;
+		power = power * BASE;
 
 	uint64_t hash = 0;
 	for (int i = 0; i < ksize - 1; ++i)
-		hash = hash * 4 + base_to_int(seq[i]);
+		hash = hash * BASE + base_to_int(seq[i]);
 	int len = strlen(seq);
 	for (int i = 0; i + ksize <= len; ++i){
 		if (i > 0)
 			hash -= power * base_to_int(seq[i - 1]);
-		hash = hash * 4 + base_to_int(seq[i + ksize - 1]);
+		hash = hash * BASE + base_to_int(seq[i + ksize - 1]);
 
 		khiter_t it = kh_get(kmer_int, h, hash);
 		if (it == kh_end(h)){
@@ -60,7 +60,7 @@ void count_hash_from_read(char *seq, int ksize, khash_t(kmer_int) *h)
 int count_kmer_on_seq(khash_t(kmer_int) *h, char *seq, int ksize)
 {
 	khash_t(kmer_int) *h1 = kh_init(kmer_int);
-	count_hash_from_read(seq, ksize, h1);
+	count_hash_from_seq(seq, ksize, h1);
 	int res = 0;
 	for (khiter_t it1 = kh_begin(h1); it1 != kh_end(h1); ++it1){
 		if (!kh_exist(h1, it1))
@@ -69,7 +69,6 @@ int count_kmer_on_seq(khash_t(kmer_int) *h, char *seq, int ksize)
 		int val = kh_val(h1, it1);
 		khiter_t it = kh_get(kmer_int, h, key);
 		if (it != kh_end(h))
-			//res += min(val, kh_val(h, it));
 			res += kh_val(h, it);
 	}
 	kh_destroy(kmer_int, h1);
@@ -80,16 +79,16 @@ void print_kmer_count_on_seq(khash_t(kmer_int) *h, char *seq, int ksize)
 {
 	uint64_t power = 1;
 	for (int i = 0; i < ksize - 1; ++i)
-		power *= 4;
+		power *= BASE;
 
 	uint64_t hash = 0;
 	for (int i = 0; i < ksize - 1; ++i)
-		hash = hash * 4 + base_to_int(seq[i]);
+		hash = hash * BASE + base_to_int(seq[i]);
 	int len = strlen(seq);
 	for (int i = 0; i + ksize <= len; ++i){
 		if (i > 0)
 			hash -= power * base_to_int(seq[i - 1]);
-		hash = hash * 4 + base_to_int(seq[i + ksize - 1]);
+		hash = hash * BASE + base_to_int(seq[i + ksize - 1]);
 		khiter_t it = kh_get(kmer_int, h, hash);
 		if (it != kh_end(h))
 			printf("%d ", kh_val(h, it));
@@ -113,7 +112,7 @@ int kmer_check(char *first, char *second, int overlap_ksize, int check_ksize,
 	strcpy(join, first + pos);
 	strcat(join, second + overlap_ksize);
 	khash_t(kmer_int) *h = kh_init(kmer_int);
-	count_hash_from_read(join, check_ksize, h);
+	count_hash_from_seq(join, check_ksize, h);
 	int res = 1;
 	for (khiter_t it = kh_begin(h); it != kh_end(h); ++it){
 		if (!kh_exist(h, it))
@@ -145,7 +144,7 @@ int count_zero_kmer_map(char *first, char *second, int overlap_ksize,
 	strncpy(join + m, second + overlap_ksize,
 			max(0, min(len2 - overlap_ksize, m)));
 	khash_t(kmer_int) *h = kh_init(kmer_int);
-	count_hash_from_read(join, check_ksize, h);
+	count_hash_from_seq(join, check_ksize, h);
 	int count = 0;
 	for (khiter_t it = kh_begin(h); it != kh_end(h); ++it){
 		if (!kh_exist(h, it))
@@ -177,7 +176,7 @@ int count_max_consecutive_zero_kmer(char *first, char *second, int overlap_ksize
 	strncpy(join + m, second + overlap_ksize,
 			min(len2, check_ksize) - overlap_ksize);
 	khash_t(kmer_int) *h = kh_init(kmer_int);
-	count_hash_from_read(join, check_ksize, h);
+	count_hash_from_seq(join, check_ksize, h);
 	int max_consecutive = 0;
 	int count = 0;
 	for (khiter_t it = kh_begin(h); it != kh_end(h); ++it){
