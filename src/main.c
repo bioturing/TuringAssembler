614d5d3c7fe6ef17dbefbd4b212427cbdfa1b5ff
JATC1024 <trananhhao1024@gmail.com>
2019-10-06 21:54:29 -0700

    Fix free opt bug

--- src/main.c
+++ src/main.c
@@ -295,7 +295,7 @@ void build_opt_process(int argc, char *argv[], void (*build_process)(struct opt_
 	init_clock();
 	print_info(argc, argv);
 	build_process(opt);
-	destroy_opt_proc_t(opt);
+	//destroy_opt_proc_t(opt); /* No need */
 }
 
 void build_0_opt_process(int argc, char *argv[])
