78548470c5d44d4a6962c24c70db6495aa9b1004
JATC1024 <trananhhao1024@gmail.com>
2019-10-15 01:15:12 -0700

    Minor changes

--- src/assembly_graph.c
+++ src/assembly_graph.c
@@ -1491,10 +1491,8 @@ void asm_clone_graph(struct asm_graph_t *g0, struct asm_graph_t *g1,
 	save_asm_graph(g0, tmp_name);
 	load_asm_graph(g1, tmp_name);
 	int status = remove(tmp_name);
-	if (status != 0){
-		log_warn("Unable to remove %s\n", tmp_name);
-		perror("The following error");
-	}
+	if (status != 0)
+		log_error("Unable to remove %s\n", tmp_name);
 	return;
 	g1->ksize = g0->ksize;
 	g1->bin_size = g0->bin_size;
