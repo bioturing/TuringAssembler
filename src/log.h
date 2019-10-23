6b691767d7cc54678b94bbfdc4c97cb820f5e7a1
JATC1024 <trananhhao1024@gmail.com>
2019-10-22 23:56:42 -0700

    Change logs in local_assembly to log_local

--- src/log.h
+++ src/log.h
@@ -26,12 +26,12 @@ enum { LOG_TRACE, LOG_DEBUG_TECH, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_
 #define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
-#define log_local_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
-#define log_local_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
-#define log_local_info(...) log_log(LOG_INFO, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
-#define log_local_warn(...) log_log(LOG_WARN, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
-#define log_local_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
-#define log_local_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, "Local %d %d " __VA_ARGS__)
+#define log_local_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
+#define log_local_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
+#define log_local_info(...) log_log(LOG_INFO, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
+#define log_local_warn(...) log_log(LOG_WARN, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
+#define log_local_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
+#define log_local_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, "[%d %d] " __VA_ARGS__)
 
 void log_set_udata(void *udata);
 void log_set_lock(log_LockFn fn);
