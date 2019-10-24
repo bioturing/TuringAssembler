4fe982dc86759aa7b109dac378051491f8c10e75
Tan <tan@bioturing.com>
2019-10-24 16:29:37 +0700

    add declaration

--- src/barcode_builder.h
+++ src/barcode_builder.h
@@ -9,6 +9,7 @@ struct asm_align_t {
 	int aligned;
 };
 
+int ksw_global2(int qlen, const uint8_t *query, int tlen, const uint8_t *target, int m, const int8_t *mat, int o_del, int e_del, int o_ins, int e_ins, int w, int *n_cigar_, uint32_t **cigar_);
 mem_opt_t *asm_memopt_init();
 struct asm_align_t asm_reg2aln(const mem_opt_t *opt, const bntseq_t *bns,
 	const uint8_t *pac, int l_query, const uint8_t *query, const mem_alnreg_t *ar);
