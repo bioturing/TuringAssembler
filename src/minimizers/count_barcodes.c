ea9678f4fa2bdf1c7f634ba7d04ed7afcdd07851
Tan <tan@bioturing.com>
2019-11-25 15:06:56 +0700

    fast hash function and doubling

--- src/minimizers/count_barcodes.c
+++ src/minimizers/count_barcodes.c
@@ -32,65 +32,22 @@
 #include "atomic.h"
 #include "log.h"
 
-pthread_mutex_t lock_key;
 
 /*
  Credit for primes table: Aaron Krowne
  http://br.endernet.org/~akrowne/
  http://planetmath.org/encyclopedia/GoodHashTablePrimes.html
  */
-static const uint64_t primes[] = { 1610612741, 2147483647,
-                                   4294967295};
-#define N_PRIMES_NUMBER 3
+static const uint64_t primes[16] = { 49157, 98317, 196613, 393241, 786433, 1572869, 3145739,
+				   6291469, 12582917, 25165843, 50331653, 100663319, 201326611, 402653189,
+                                   805306457, 1610612741};
+#define N_PRIMES_NUMBER 16
 
-#define MAX_LOAD_FACTOR 0.9
+#define MAX_LOAD_FACTOR 0.65
 #define FATAL_LOAD_FACTOR 0.9
-#define BIG_CONSTANT(x) (x##LLU)
+#define KEY_LEN 8
 
-#if defined(_MSC_VER)
 
-#define FORCE_INLINE	__forceinline
-
-#include <stdlib.h>
-
-#define ROTL32(x,y)	_rotl(x,y)
-#define ROTL64(x,y)	_rotl64(x,y)
-
-#define BIG_CONSTANT(x) (x)
-
-// Other compilers
-
-#else	// defined(_MSC_VER)
-
-#define        FORCE_INLINE inline __attribute__((always_inline))
-
-inline uint32_t rotl32(uint32_t x, int8_t r)
-{
-	return (x << r) | (x >> (32 - r));
-}
-
-inline uint64_t rotl64(uint64_t x, int8_t r)
-{
-	return (x << r) | (x >> (64 - r));
-}
-
-#define        ROTL32(x, y)        rotl32(x,y)
-#define ROTL64(x, y)        rotl64(x,y)
-
-#define BIG_CONSTANT(x) (x##LLU)
-
-#endif // !defined(_MSC_VER)
-
-static inline uint64_t fmix64(uint64_t k)
-{
-	k ^= k >> 33;
-	k *= BIG_CONSTANT(0xff51afd7ed558ccd);
-	k ^= k >> 33;
-	k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
-	k ^= k >> 33;
-
-	return k;
-}
 
 struct readbc_t {
     uint64_t barcode;
@@ -122,23 +79,24 @@ struct mini_hash_t {
  */
 struct mini_hash_t *init_mini_hash(uint32_t p_index)
 {
-	struct mini_hash_t *h_table = malloc(sizeof(struct mini_hash_t));
-	h_table->prime_index = p_index;
-	uint32_t h_size = primes[h_table->prime_index];
-	h_table->h = calloc(h_size, sizeof(uint64_t));
-	h_table->key = calloc(h_size, sizeof(uint64_t));
-	h_table->size = h_size;
-	h_table->max_cnt = (uint64_t) (h_table->size * MAX_LOAD_FACTOR);
-	h_table->count = 0;
-	memset(h_table->h, 0, h_table->size);
-	memset(h_table->key, 0, h_table->size);
-	return h_table;
+	struct mini_hash_t *table = malloc(sizeof(struct mini_hash_t));
+	table->prime_index = p_index;
+	uint32_t h_size = primes[table->prime_index];
+	table->h = calloc(h_size, sizeof(uint64_t));
+	table->key = calloc(h_size, sizeof(uint64_t));
+	table->size = h_size;
+	table->max_cnt = (uint64_t) (table->size * MAX_LOAD_FACTOR);
+	table->count = 0;
+	memset(table->h, 0, table->size);
+	memset(table->key, 0, table->size);
+	return table;
 }
 
 void destroy_mini_hash(struct mini_hash_t *h_table)
 {
 	free(h_table->h);
 	free(h_table->key);
+	pthread_mutex_destroy(&h_table->mut);
 	free(h_table);
 }
 
@@ -181,101 +139,38 @@ static inline uint64_t get_barcode_biot(char *s, struct read_t *r)
 	return ret;
 }
 
-static inline uint64_t getblock64(const uint64_t *p, int i)
-{
-	return p[i];
+/*
+ * Thomas Wang 64 bit mix hash function
+ */
+
+uint64_t twang_mix64(uint64_t key) {
+	key = (~key) + (key << 21); // key *= (1 << 21) - 1; key -= 1;
+	key = key ^ (key >> 24);
+	key = key + (key << 3) + (key << 8); // key *= 1 + (1 << 3) + (1 << 8)
+	key = key ^ (key >> 14);
+	key = key + (key << 2) + (key << 4); // key *= 1 + (1 << 2) + (1 << 4)
+	key = key ^ (key >> 28);
+	key = key + (key << 31); // key *= 1 + (1 << 31)
+	return key;
 }
 
-static inline uint64_t MurmurHash3_x64_64(const uint8_t *data, const int len)
-{
-	int n_blocks = len >> 4;
-	uint64_t h1 = BIG_CONSTANT(0x13097);
-	uint64_t h2 = BIG_CONSTANT(0x13097);
+/*
+ * Inverse of twang_mix64
+ *
+ * Note that twang_unmix64 is significantly slower than twang_mix64.
+ */
 
-	const uint64_t c1 = BIG_CONSTANT(0x87c37b91114253d5);
-	const uint64_t c2 = BIG_CONSTANT(0x4cf5ad432745937f);
-
-	const uint64_t *blocks = (const uint64_t *) (data);
-
-	int i;
-	for (i = 0; i < n_blocks; ++i) {
-		uint64_t k1 = getblock64(blocks, i << 1);
-		uint64_t k2 = getblock64(blocks, (i << 1) + 1);
-		k1 *= c1;
-		k1 = ROTL64(k1, 31);
-		k1 *= c2;
-		h1 ^= k1;
-		h1 = ROTL64(h1, 27);
-		h1 += h2;
-		h1 = h1 * 5 + 0x52dce729;
-		k2 *= c2;
-		k2 = ROTL64(k2, 33);
-		k2 *= c1;
-		h2 ^= k2;
-		h2 = ROTL64(h2, 31);
-		h2 += h1;
-		h2 = h2 * 5 + 0x38495ab5;
-	}
-
-	const uint8_t *tail = (const uint8_t *) (data + (n_blocks << 4));
-	uint64_t k1 = 0;
-	uint64_t k2 = 0;
-
-	switch (len & 15) {
-		case 15:
-			k2 ^= ((uint64_t) tail[14]) << 48;
-		case 14:
-			k2 ^= ((uint64_t) tail[13]) << 40;
-		case 13:
-			k2 ^= ((uint64_t) tail[12]) << 32;
-		case 12:
-			k2 ^= ((uint64_t) tail[11]) << 24;
-		case 11:
-			k2 ^= ((uint64_t) tail[10]) << 16;
-		case 10:
-			k2 ^= ((uint64_t) tail[9]) << 8;
-		case 9:
-			k2 ^= ((uint64_t) tail[8]) << 0;
-			k2 *= c2;
-			k2 = ROTL64(k2, 33);
-			k2 *= c1;
-			h2 ^= k2;
-
-		case 8:
-			k1 ^= ((uint64_t) tail[7]) << 56;
-		case 7:
-			k1 ^= ((uint64_t) tail[6]) << 48;
-		case 6:
-			k1 ^= ((uint64_t) tail[5]) << 40;
-		case 5:
-			k1 ^= ((uint64_t) tail[4]) << 32;
-		case 4:
-			k1 ^= ((uint64_t) tail[3]) << 24;
-		case 3:
-			k1 ^= ((uint64_t) tail[2]) << 16;
-		case 2:
-			k1 ^= ((uint64_t) tail[1]) << 8;
-		case 1:
-			k1 ^= ((uint64_t) tail[0]) << 0;
-			k1 *= c1;
-			k1 = ROTL64(k1, 31);
-			k1 *= c2;
-			h1 ^= k1;
-	}
-
-	h1 ^= len;
-	h2 ^= len;
-
-	h1 += h2;
-	h2 += h1;
-
-	h1 = fmix64(h1);
-	h2 = fmix64(h2);
-
-	h1 += h2;
-	h2 += h1;
-
-	return h2;
+uint64_t twang_unmix64(uint64_t key) {
+	// See the comments in jenkins_rev_unmix32 for an explanation as to how this
+	// was generated
+	key *= 4611686016279904257U;
+	key ^= (key >> 28) ^ (key >> 56);
+	key *= 14933078535860113213U;
+	key ^= (key >> 14) ^ (key >> 28) ^ (key >> 42) ^ (key >> 56);
+	key *= 15244667743933553977U;
+	key ^= (key >> 24) ^ (key >> 48);
+	key = (key + 1) * 9223367638806167551U;
+	return key;
 }
 
 /**
@@ -287,7 +182,7 @@ void mini_expand()
 {
 	uint32_t i;
 	uint32_t slot, prev;
-	uint64_t val, key;
+	uint64_t val, key, data;
 	uint32_t log_size = 1<<16;
 	struct mini_hash_t *new_table = init_mini_hash(h_table->prime_index + 1);
 	for (i = 0; i < h_table->size; ++i) {
@@ -298,7 +193,8 @@ void mini_expand()
 		if (!h_table->h[i])
 			continue;
 		val = h_table->h[i];
-		key = h_table->key[i];
+		data = h_table->key[i];
+		key = twang_mix64(data);
 		slot = prev = key % new_table->size;
 		while (new_table->key[slot] != 0 && slot < new_table->size) {
 			slot++;
@@ -309,7 +205,7 @@ void mini_expand()
 				slot++;
 			}
 		}
-		new_table->key[slot] = key;
+		new_table->key[slot] = data;
 		new_table->h[slot] = val;
 		new_table->count++;
 	}
@@ -387,7 +283,7 @@ int mini_inc_by_key(uint64_t data, uint64_t key)
 
 uint64_t mini_get(uint64_t data, uint64_t key)
 {
-	uint64_t mask = h_table->size - 1;
+	uint64_t mask = h_table->size;
 	uint64_t slot = key % mask;
 	while (h_table->key[slot] != data && h_table->key[slot] != 0 && slot != h_table->size) {
 		printf("slot %ld . h_table key %ld \n", slot, h_table->key[slot]);
@@ -406,7 +302,7 @@ uint64_t mini_get(uint64_t data, uint64_t key)
  */
 void mini_inc(uint64_t data, int len)
 {
-	uint64_t key = MurmurHash3_x64_64((uint8_t *) &data, len);
+	uint64_t key = twang_mix64(data);
 	if (h_table->count > h_table->max_cnt) {
 		try_expanding();
 	}
@@ -453,7 +349,6 @@ static inline void *biot_buffer_iterator_simple(void *data);
  */
 void count_bx_freq(struct opt_proc_t *opt, struct read_path_t *r_path)
 {
-	pthread_mutex_init(&lock_key, NULL);
 	h_table = init_mini_hash(0);
 
 	pthread_attr_t attr;
