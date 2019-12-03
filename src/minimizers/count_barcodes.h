1b225627194f0230d5a79703f8a904ee82720c8c
Tan <tan@bioturing.com>
2019-12-04 01:22:10 +0700

    some how generic

--- src/minimizers/count_barcodes.h
+++ src/minimizers/count_barcodes.h
@@ -8,7 +8,7 @@
 #include <stdint.h>
 
 struct mini_hash_t {
-    uint32_t *h;
+    uint64_t *h;
     uint64_t *key;
     uint64_t size;
     uint64_t count;
@@ -20,7 +20,7 @@ void count_bx_freq(struct opt_proc_t *opt, struct read_path_t *r_path);
 uint64_t barcode_hash_mini(char *s);
 void init_mini_hash(struct mini_hash_t **h_table, uint32_t p_index);
 void destroy_mini_hash(struct mini_hash_t *h_table);
-int mini_inc_by_key(struct mini_hash_t *h_table, uint64_t data, uint64_t key);
+int mini_inc_by_key(struct mini_hash_t *h_table, uint64_t data, uint64_t key, void * (*func)(void *), void *arg);
 uint64_t mini_get(struct mini_hash_t *h_table, uint64_t data, uint64_t key);
 
 #endif //SKIPPING_COUNT_BARCODES_H
