4bdc946463f501302db691bd0da909b8a5ebda08
Tan <tan@bioturing.com>
2019-12-08 18:31:02 +0700

    add index kmer impementation

---
+++ src/coverage/kmer_count.d
@@ -0,0 +1,17 @@
+src/coverage/kmer_count.o: src/coverage/kmer_count.c \
+ src/coverage/../assembly_graph.h src/coverage/../attribute.h \
+ src/coverage/../pthread_barrier.h src/coverage/../barcode_hash.h \
+ src/coverage/../asm_hash.h src/coverage/../khash.h \
+ src/coverage/../radix_sort.h src/coverage/../minimizers/minimizers.h \
+ src/coverage/../minimizers/../khash.h \
+ src/coverage/../minimizers/../assembly_graph.h \
+ src/coverage/../minimizers/count_barcodes.h \
+ src/coverage/../minimizers/../cluster_molecules.h \
+ src/coverage/../minimizers/../assembly_graph.h \
+ src/coverage/../minimizers/../minimizers/minimizers.h \
+ src/coverage/../minimizers/../sort_read.h \
+ src/coverage/../minimizers/../attribute.h \
+ src/coverage/../minimizers/../khash.h \
+ src/coverage/../minimizers/../get_buffer.h \
+ src/coverage/../minimizers/../simple_queue.h \
+ src/coverage/../minimizers/../khash_operations.h src/coverage/../log.h
