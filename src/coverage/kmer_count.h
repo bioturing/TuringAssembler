9eb2ec8b0c5fffb23b6979e95e837031624b96b4
Tan <tan@bioturing.com>
2019-12-08 19:37:14 +0700

    can add count

--- src/coverage/kmer_count.h
+++ src/coverage/kmer_count.h
@@ -7,4 +7,5 @@
 
 struct mini_hash_t * construct_edges_hash(struct asm_graph_t *g);
 struct mini_hash_t *kmer_count_on_edges(struct opt_proc_t *opt);
+void add_cnt_to_graph(struct asm_graph_t *g, struct mini_hash_t *kmer_table);
 #endif //SKIPPING_KMER_COUNT_H
