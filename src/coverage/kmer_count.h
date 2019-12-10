61470cb448da680ad48559e1147b6fddb6686023
Tan <tan@bioturing.com>
2019-12-10 22:33:53 +0700

    wrap-up to assembly3 process

--- src/coverage/kmer_count.h
+++ src/coverage/kmer_count.h
@@ -6,6 +6,6 @@
 #define SKIPPING_KMER_COUNT_H
 
 struct mini_hash_t * construct_edges_hash(struct asm_graph_t *g);
-struct mini_hash_t *kmer_count_on_edges(struct opt_proc_t *opt);
+struct mini_hash_t *kmer_count_on_edges(struct opt_proc_t *opt, struct asm_graph_t *g);
 void add_cnt_to_graph(struct asm_graph_t *g, struct mini_hash_t *kmer_table);
 #endif //SKIPPING_KMER_COUNT_H
