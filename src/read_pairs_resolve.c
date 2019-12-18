b3287d394f7ff254177005f5b16ef2148c595f45
JATC1024 <trananhhao1024@gmail.com>
2019-12-18 00:19:49 -0800

    Able to go both side

--- src/read_pairs_resolve.c
+++ src/read_pairs_resolve.c
@@ -60,7 +60,8 @@ int get_next_cand(struct asm_graph_t *g, float unit_cov, struct read_pair_cand_t
 		int v = rp_cand[last].cand[i];
 		float cov = __get_edge_cov(g->edges + v, g->ksize);
 		if (cov >= 0.25 * unit_cov
-			&& rp_cand[last].score[i] >= MIN_READ_PAIR_MAPPED_HARD)
+			&& rp_cand[last].score[i] >= MIN_READ_PAIR_MAPPED_HARD
+			&& g->edges[v].seq_len >= 100)
 			kh_set_int_insert(cand, rp_cand[last].cand[i]);
 	}
 
@@ -71,7 +72,8 @@ int get_next_cand(struct asm_graph_t *g, float unit_cov, struct read_pair_cand_t
 		for (int i = 0; i < rp_cand[v].n; ++i){
 			int u = rp_cand[v].cand[i];
 			if (rp_cand[v].score[i] >= MIN_READ_PAIR_MAPPED_SOFT
-					&& kh_set_int_exist(cand, u))
+				&& kh_set_int_exist(cand, u)
+				&& g->edges[u].seq_len >= 100)
 				kh_set_int_insert(new_cand, u);
 		}
 		kh_destroy(set_int, cand);
@@ -120,31 +122,40 @@ void get_long_contigs(struct opt_proc_t *opt)
 	load_asm_graph(g, opt->in_file);
 	struct read_pair_cand_t *rp_cand = calloc(g->n_e, sizeof(struct read_pair_cand_t));
 	get_read_pairs_count(g, opt->in_fasta, rp_cand);
-	
+
 	uint64_t *edge_sorted = calloc(g->n_e, sizeof(uint64_t));
 	for (int i = 0; i < g->n_e; ++i)
 		edge_sorted[i] = GET_CODE(i, g->edges[i].seq_len);
 	qsort(edge_sorted, g->n_e, sizeof(uint64_t), &cmp_edge_length);
 
 	float unit_cov = get_genome_coverage(g);
+	int *visited = calloc(g->n_e, sizeof(int));
 	struct asm_graph_t *g_new = calloc(1, sizeof(struct asm_graph_t));
+	int n_e = 0;
+	struct asm_edge_t *edges = NULL;
 	for (int i = g->n_e - 1, j = 0; i >= 0 && j < 10; --i, ++j){
 		int v = edge_sorted[i] >> 32;
 		float cov = __get_edge_cov(g->edges + v, g->ksize);
-		if (cov < 0.25 * unit_cov)
+		if (cov < 0.25 * unit_cov || g->edges[v].seq_len < 100)
 			continue;
-		int *path;
-		int n_path;
-		extend_by_read_pairs(g, v, unit_cov, rp_cand, &path, &n_path);
-
-		for (int j = 0; j < n_path; ++j)
-			__VERBOSE("%d ", path[j]);
-		__VERBOSE("\n");
-		for (int j = 0; j < n_path; ++j){
-			float cov = __get_edge_cov(g->edges + path[j], g->ksize);
-			__VERBOSE("%.3f ", cov);
-		}
-		__VERBOSE("\n");
+		int *path_fw, *path_rv;
+		int n_path_fw, n_path_rv;
+		extend_by_read_pairs(g, v, unit_cov, rp_cand, &path_fw, &n_path_fw);
+		extend_by_read_pairs(g, g->edges[v].rc_id, unit_cov, rp_cand,
+				&path_rv, &n_path_rv);
+		int *path = calloc(n_path_fw + n_path_rv - 1, sizeof(int));
+		int n_path = 0;
+		for (int i = n_path_rv - 1; i >= 0; --i)
+			path[n_path++] = g->edges[path_rv[i]].rc_id;
+		for (int i = 1; i < n_path_fw; ++i)
+			path[n_path++] = path_fw[i];
+		free(path_fw);
+		free(path_rv);
+		/*__VERBOSE("Start from %d, fw: %d, rv: %d\n", v, n_path_fw,
+				n_path_rv);
+		for (int i = 0; i < n_path; ++i)
+			__VERBOSE("%d ", path[i]);
+		__VERBOSE("\n");*/
 	}
 
 	asm_graph_destroy(g);
