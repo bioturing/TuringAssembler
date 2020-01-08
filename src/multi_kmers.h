15bfb6ad7d398a3df5c23e9246df9554a0867c73
Huu <chequochuu@gmail.com>
2020-01-08 18:23:48 +0700

    fix

---
+++ src/multi_kmers.h
@@ -0,0 +1,18 @@
+//
+// Created by che on 08/01/2020.
+//
+
+#ifndef SKIPPING_MULTI_KMERS_H
+#define SKIPPING_MULTI_KMERS_H
+
+#include "assembly_graph.h"
+void build_graph_from_scratch(int ksize, int n_threads, int mmem, int n_files,
+			      char **files_1, char **files_2, char *work_dir,
+			      struct asm_graph_t *g);
+void load_asm_graph(struct asm_graph_t *g, const char *path);
+void resolve_multi_kmer(struct opt_proc_t *opt, struct asm_graph_t *g, int lastk);
+void save_graph_info(const char *out_dir, struct asm_graph_t *g, const char *suffix);
+void resolve_graph_operation(struct asm_graph_t *g0, struct asm_graph_t *g);
+void init_logger(int level, const char * file_path);
+void set_log_stage(char *stage);
+#endif //SKIPPING_MULTI_KMERS_H
