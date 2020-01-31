37b2538dd684f909d6e0edcbee946111bff04981
jatc <jatc1024@gmail.com>
2020-01-31 18:13:37 +0700

    Off huu's reduce read process

--- src/main.c
+++ src/main.c
@@ -467,8 +467,8 @@ int main(int argc, char *argv[])
 		build_opt_process(argc, argv, &resolve_complex_bulges_process);
 	else if (!strcmp(argv[1], "debug_split_molecules"))
 		build_opt_process(argc, argv, &split_molecules_wrapper);
-	else if (!strcmp(argv[1], "reduce_reads"))
-		reduce_read_opt_process(argc, argv);
+	//else if (!strcmp(argv[1], "reduce_reads"))
+	//	reduce_read_opt_process(argc, argv);
 	else if (!strcmp(argv[1], "resolve_local"))
 		resolve_local_opt_process(argc, argv);
 	else if (!strcmp(argv[1], "build_scaffolding_1_2"))
